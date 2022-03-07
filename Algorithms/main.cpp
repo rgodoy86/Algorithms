5736baa088a198517e706d1aed279ef429668b6d
rgodoy86 <rgodoy86@gmail.com>
2022-03-07 21:02:37 +0100

    bubble sort created

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -6,9 +6,71 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+// Function to print vector values
+template<typename T>
+void printv(vector<T> &v){
+    if(v.empty()) return;
+    
+    for(T &item : v){
+        cout << item << " ";
+    }
+    cout << endl;
+}
+
+
+// Sort vector using Bubble technique
+template<typename T>
+vector<T> sort_bubble(vector<T> &v){
+    T value_tmp;
+    
+    if(v.empty()) return v;
+    
+    unsigned long repeat = v.size()-1;
+    unsigned long last_index = v.size()-1;
+    
+    while(repeat > 0){
+        for(int i=0; i < last_index; ++i){
+            if(v.at(i) > v.at(i+1)){
+                value_tmp = v.at(i+1);
+                v.at(i+1) = v.at(i);
+                v.at(i) = value_tmp;
+            }
+        }
+        --repeat;
+        --last_index;
+    }
+    
+    return v;
+    
+}
+
+
+
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<int> v1;
+    
+    // Create vector with random numbers
+    srand((int)time(0));
+    int i = 0;
+    while(i++ < 10){
+        v1.push_back(rand() % 100);
+    }
+    
+    printv(v1);
+    
+    sort_bubble(v1);
+    
+    printv(v1);
+    
+    
+    
+    
     return 0;
 }
