1c0db343385df60d899aefb93d34a062ed4ebebd
rgodoy86 <rgodoy86@gmail.com>
2022-03-07 22:05:22 +0100

    Merge sort function added

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -24,12 +24,12 @@ void printv(vector<T> &v){
 }
 
 
-// Sort vector using Bubble technique
+// Bubble Sort
 template<typename T>
-vector<T> sort_bubble(vector<T> &v){
+void sort_bubble(vector<T> &v){
     T value_tmp;
     
-    if(v.empty()) return v;
+    if(v.empty()) return;
     
     unsigned long repeat = v.size()-1;
     unsigned long last_index = v.size()-1;
@@ -45,12 +45,71 @@ vector<T> sort_bubble(vector<T> &v){
         --repeat;
         --last_index;
     }
-    
-    return v;
-    
 }
 
+// Merge Sort
+// O(n logn) time
+// O(n) space
+template <typename T>
+void merge(vector<T> &v, vector<T> &v_left, vector<T> &v_right){
+    unsigned long  v_size = v.size();
+    unsigned long  v_left_size = v_left.size();
+    unsigned long  v_right_size = v_right.size();
+    unsigned long  i_left=0, i_right=0, i_merged=0;
+    
+    // Compare the first indexes and see which one is the smalles one
+    while(i_left < v_left_size && i_right < v_right_size){
+        if(v_left.at(i_left) <= v_right.at(i_right)){
+            v.at(i_merged) = v_left.at(i_left);
+            ++i_left;
+        }else{
+            v.at(i_merged) = v_right.at(i_right);
+            ++i_right;
+        }
+        ++i_merged;
+    }
+    
+    // Fill the remaining left part
+    while(i_left < v_left_size){
+        v.at(i_merged) = v_left.at(i_left);
+        ++i_left;
+        ++i_merged;
+    }
+    
+    // Or fill the remianing right part
+    while(i_right < v_right_size){
+        v.at(i_merged) = v_right.at(i_right);
+        ++i_right;
+        ++i_merged;
+    }
+}
 
+template <typename T>
+void sort_merge(vector<T> &v){
+    unsigned long vector_size = v.size();
+    
+    if(vector_size < 2) return;
+    
+    unsigned long index_mid = vector_size / 2;
+    
+    vector<T> v_left(index_mid);
+    vector<T> v_right(vector_size - index_mid);
+    
+    // Split into vector_left and vector_right
+    for(int i = 0; i < vector_size; ++i){
+        if (i < index_mid){
+            v_left.at(i) = v.at(i);
+        }else{
+            v_right.at(i-index_mid) = v.at(i);
+        }
+    }
+    
+    // Split each vector
+    sort_merge(v_left);
+    sort_merge(v_right);
+    
+    merge(v, v_left, v_right);
+}
 
 int main() {
     // insert code here...
@@ -63,14 +122,18 @@ int main() {
         v1.push_back(rand() % 100);
     }
     
+    auto v2 = v1;
+    
+    cout << "Original Vector: ";
     printv(v1);
     
+    cout << "Sort Bubble: ";
     sort_bubble(v1);
-    
     printv(v1);
     
-    
-    
+    cout << "Sort Merge:  ";
+    sort_merge(v2);
+    printv(v2);
     
     return 0;
 }
